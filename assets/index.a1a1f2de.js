a1a37ef3d953a4fdd29f41bd83f6d50b7ca438ea
Daniel Nasr <danialnasronline@gmail.com>
2022-09-10 12:19:20 +0430

    init

---
+++ assets/index.a1a1f2de.js
@@ -0,0 +1,41 @@
+(function(){const r=document.createElement("link").relList;if(r&&r.supports&&r.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))a(e);new MutationObserver(e=>{for(const t of e)if(t.type==="childList")for(const s of t.addedNodes)s.tagName==="LINK"&&s.rel==="modulepreload"&&a(s)}).observe(document,{childList:!0,subtree:!0});function o(e){const t={};return e.integrity&&(t.integrity=e.integrity),e.referrerpolicy&&(t.referrerPolicy=e.referrerpolicy),e.crossorigin==="use-credentials"?t.credentials="include":e.crossorigin==="anonymous"?t.credentials="omit":t.credentials="same-origin",t}function a(e){if(e.ep)return;e.ep=!0;const t=o(e);fetch(e.href,t)}})();document.querySelector("#app").innerHTML=`
+
+<h1 class="text-center mb-2 text-2xl md:text-4xl rtl"> \u0634\u0631\u0648\u0639 \u062A\u0648\u0633\u0639\u0647 \u0648\u0628 ( \u0641\u0631\u0627\u0646\u062A \u0627\u0646\u062F ) </h1>
+
+<div class="grid grid-cols-1 place-items-center sm:grid-cols-2 md:grid-cols-4 min-h-screen">
+
+<a href="https://free-learn.ir/category/web_design/html_tutorial/">HTML</a>
+<a href="https://free-learn.ir/category/web_design/css_tutorial/">CSS</a>
+<a href="https://toplearn.com/c/Kr8Y">HTML & CSS ( \u0648\u06CC\u062F\u06CC\u0648 )</a>
+<a href="https://free-learn.ir/category/web_design/javascript_tutorial/">JavaScript</a>
+<a href="https://sabzlearn.ir/product/java-script-zero-to-hero/">JavaScript ( \u0648\u06CC\u062F\u06CC\u0648 )</a>
+<a href="https://www.w3schools.com/html/default.asp">HTML</a>
+<a href="https://www.w3schools.com/css/default.asp">CSS</a>
+<a href="https://www.w3schools.com/js/default.asp">JavaScript</a>
+<a href="https://developer.mozilla.org/en-US/docs/Web/HTML">HTML</a>
+<a href="https://developer.mozilla.org/en-US/docs/Web/CSS">CSS</a>
+<a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript">JavaScript</a>
+<a href="">JavaScript ( \u0648\u06CC\u062F\u06CC\u0648 )</a>
+<a href="https://startbootstrap.com/">Bootstrap</a>
+<a href="https://getbootstrap.com/docs/5.2/getting-started/introduction/">Bootstrap</a>
+<a href="https://www.w3schools.com/bootstrap5/index.php">Bootstrap</a>
+<a href="https://roocket.ir/series/learn-bootstrap-4">Bootstrap ( \u0648\u06CC\u062F\u06CC\u0648 )</a>
+<a href="https://maktabkhooneh.org/course/%D8%A2%D9%85%D9%88%D8%B2%D8%B4-%D8%B1%D8%A7%DB%8C%DA%AF%D8%A7%D9%86-%D8%A8%D9%88%D8%AA-%D8%A7%D8%B3%D8%AA%D8%B1%D9%BE-5-bootstrap-mk1205/">Bootstrap ( \u0648\u06CC\u062F\u06CC\u0648 )</a>
+<a href="https://www.w3schools.com/jquery/default.asp">jQuery</a>
+<a href="https://toplearn.com/courses/60/%D8%AF%D9%88%D8%B1%D9%87-%D8%AC%D8%A7%D9%85%D8%B9-%D9%81%D8%B1%DB%8C%D9%85-%D9%88%D8%B1%DA%A9-%D8%AC%DB%8C-%DA%A9%D9%88%D8%A6%D8%B1%DB%8C">jQuery ( \u0648\u06CC\u062F\u06CC\u0648 )</a>
+<a href="https://vuejs.org/">VueJs</a>
+<a href="https://webprog.io/course/%D8%A2%D9%85%D9%88%D8%B2%D8%B4-vue-js-3-%D9%BE%D8%B1%D9%88%DA%98%D9%87-%D9%85%D8%AD%D9%88%D8%B1">VueJs ( \u0648\u06CC\u062F\u06CC\u0648 )</a>
+<a href="https://sabzlearn.ir/product/free-vuejs-training/">VueJs ( \u0648\u06CC\u062F\u06CC\u0648 )</a>
+<a href="https://vitejs.dev/">Vite</a>
+<a href="https://tailwindcss.com/">Tailwind Css</a>
+<a href="https://roocket.ir/series/learn-tailwindcss">Tailwind Css ( \u0648\u06CC\u062F\u06CC\u0648 )</a>
+<a href="https://webprog.io/course/%D8%A2%D9%85%D9%88%D8%B2%D8%B4-%D8%AA%DB%8C%D9%84%D9%88%DB%8C%D9%86%D8%AF-Tailwindcss-%D9%BE%D8%B1%D9%88%DA%98%D9%87-%D9%85%D8%AD%D9%88%D8%B1">Tailwind Css ( \u0648\u06CC\u062F\u06CC\u0648 )</a>
+<a href="https://toplearn.com/c/5742">Tailwind Css ( \u0648\u06CC\u062F\u06CC\u0648 )</a>
+<a href="https://webprog.io/course/%D8%A2%D9%85%D9%88%D8%B2%D8%B4-%D8%B1%D8%A7%DB%8C%DA%AF%D8%A7%D9%86-%DA%AF%DB%8C%D8%AA-%D9%88-%DA%AF%DB%8C%D8%AA-%D9%87%D8%A7%D8%A8-git-github">Git & GitHub ( \u0648\u06CC\u062F\u06CC\u0648 )</a>
+<a href="https://nuxtjs.org/">Nuxt.Js</a>
+<a href="https://sass-lang.com/">SCSS</a>
+<a href="https://toplearn.com/c/AEO">SCSS ( \u0648\u06CC\u062F\u06CC\u0648 )</a>
+<a href="https://roocket.ir/series/learning-sass">SCSS ( \u0648\u06CC\u062F\u06CC\u0648 )</a>
+</div>
+
+`;
